f2275c4bf8ab7e30128656794cc28604d1262da5
VinodReddyBheemanagouda <vinodreddybheemanagouda@gmail.com>
2025-02-05 23:34:18 +0530

    again commit

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,7 +1,9 @@
 const apiKey = "caa35d690256898331c06ec68e529cb1";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search imput");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon=document.querySelector(".Weather-icon")
 async function checkWeather() {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   var data = await response.json();
@@ -10,7 +12,16 @@ async function checkWeather() {
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src= "images/Clouds.png"
+    }
+    else if
+        
 }
 
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
 checkWeather();
